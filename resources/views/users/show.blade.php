8d909152a68ee3c8e7bc03ead7d98fde9b60a730
panda <panbee@163.com>
2017-10-18 16:14:31 +0000

    Add user following

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,13 +4,19 @@
 <div class="row">
   <div class="col-md-offset-2 col-md-8">
     <div class="col-md-12">
-      <div class="col-md-2 col-md-8">
+      <div class="col-md-offset-2 col-md-8">
         <section class="user_info">
           @include('shared._user_info',['user' => $user])
+        </section>
+        <section class="stats">
+          @include('shared._stats',['user'=>$user])
         </section>
       </div>
     </div>
     <div class="col-md-12">
+      @if(Auth::check())
+        @include('users._follow_form')
+      @endif
       @if(count($statuses) > 0)
         <ol class="statuses">
           @foreach($statuses as $status)
